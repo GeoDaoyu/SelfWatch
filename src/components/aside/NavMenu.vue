1972c8101cb29924097e5598f2f51dd877546cb0
zhanggy <geodaoyu@foxmail.com>
2018-10-19 09:30:44 +0800

    rewrite NavMenu.vue read $route to fill NavMenu

--- src/components/aside/NavMenu.vue
+++ src/components/aside/NavMenu.vue
@@ -5,31 +5,12 @@
             default-active="1"
             router
             text-color="#A2A8B5">
-                <el-menu-item index="/system/home" >
+                <el-menu-item 
+                v-for="item in items" 
+                :key="item.name"
+                :index="item.path">
                     <i class="el-icon-location-outline"></i>
-                    <span slot="title">Home</span>
-                </el-menu-item>
-                <el-menu-item index="/system/diet">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">Diet</span>
-                </el-menu-item>
-                <el-menu-item index="/system/sleep">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">Sleep</span>
-                </el-menu-item>
-                <el-submenu index="/sub"> <!-- useless index,however without it, will call a warning -->
-                    <template slot="title">
-                        <i class="el-icon-location"></i>
-                        <span>Activity</span>
-                    </template>
-                    <el-menu-item-group>
-                        <el-menu-item index="/system/sport"><i class="el-icon-menu"></i>Sport</el-menu-item>
-                        <el-menu-item index="/system/study"><i class="el-icon-menu"></i>Study</el-menu-item>
-                    </el-menu-item-group>
-                </el-submenu>
-                <el-menu-item index="/system/filemanage">
-                    <i class="el-icon-menu"></i>
-                    <span slot="title">FileManage</span>
+                    <span>{{ item.name }}</span>
                 </el-menu-item>
             </el-menu>
         </el-col>
@@ -39,8 +20,10 @@
 <script>
   export default {
     name: "NavMenu", 
-    methods: {
-
+    data () {
+        return {
+            items: this.$router.options.routes[1].children
+        }
     }
   }
 </script>
