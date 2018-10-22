276bd43241f9a1031cd60fa095d3b86808fe1ce7
zhanggy <geodaoyu@foxmail.com>
2018-10-22 15:18:35 +0800

    MsgDialog.vue goTo & markRead methods

--- src/components/MsgDialog.vue
+++ src/components/MsgDialog.vue
@@ -1,24 +1,24 @@
 <template>
-<transition name="slide-fade">
-  <div role="alert" class="el-notification right" style="bottom: 16px; z-index: 2000;" v-show="visible">
-    <div class="el-notification__group">
-      <h2 class="el-notification__title">消息提醒</h2>
-      <div class="el-notification__content">
-        <el-table :data="messages" empty-text=" " :show-header=false style="width: 100%">
-          <el-table-column lable="内容" prop="content" min-width="220px" :formatter="ctrlContentLength"></el-table-column>
-          <el-table-column lable="操作" width="180px">
-              <template slot-scope="scope">
-              <el-button plain size="mini" type="primary">查看</el-button>
-              <el-button plain size="mini" type="warning">标为已读</el-button>
-              </template>
-          </el-table-column>
-        </el-table>
-        <a href="" class="msgAll">查看全部>></a>
+  <transition name="slide-fade">
+    <div role="alert" class="el-notification right" style="bottom: 16px; z-index: 2000;" v-show="visible">
+      <div class="el-notification__group">
+        <h2 class="el-notification__title">消息提醒</h2>
+        <div class="el-notification__content">
+          <el-table :data="messages" empty-text=" " :show-header=false style="width: 100%">
+            <el-table-column lable="内容" prop="content" min-width="220px" :formatter="ctrlContentLength"></el-table-column>
+            <el-table-column lable="操作" width="180px">
+                <template slot-scope="scope">
+                <el-button plain size="mini" type="primary" @click="goTo(scope.$index, scope.row)">查看</el-button>
+                <el-button plain size="mini" type="warning" @click="markRead(scope.$index, scope.row)">标为已读</el-button>
+                </template>
+            </el-table-column>
+          </el-table>
+          <a href="JavaScript:void(0)" class="msgAll">查看全部>></a>
+        </div>
+        <div class="el-notification__closeBtn el-icon-close" @click="visible = false"></div>
       </div>
-      <div class="el-notification__closeBtn el-icon-close" @click="closeNotification"></div>
     </div>
-  </div>
-</transition>
+  </transition>
 </template>
 
 <script>
@@ -40,27 +40,30 @@ export default {
         {
           id: 1,
           state: '未读',
-          content: '你妈叫你回家吃饭'
+          content: '你妈叫你回家吃饭',
+          toPath: 'url'
         }, {
           id: 11,
           state: '未读',
-          content: '你爸叫你带瓶酱油回来'
+          content: '你爸叫你带瓶酱油回来',
+          toPath: 'url'
         }, {
           id: 12,
           state: '未读',
-          content: '我是一条超级长的消息提醒我是一条超级长的消息提醒我是一条超级长的消息提醒'
+          content: '我是一条超级长的消息提醒我是一条超级长的消息提醒我是一条超级长的消息提醒',
+          toPath: 'url'
         }
       ]
-      this.showNotification()
-    },
-    //#region Notification
-    showNotification () {
       this.visible = true
     },
-    closeNotification () {
-      this.visible = false
+    goTo (index, row) {
+      console.log(row.toPath)
+      this.markRead(index, row)
     },
-    //#endregion
+    markRead (index, row) {
+      console.log(row.id)
+      this.messages.splice(index, 1)
+    },
     //#region table
     ctrlContentLength (row, column, cellValue, index) {
       const maxLength = 12
@@ -68,11 +71,17 @@ export default {
     }
     //#endregion
   },
-
   mounted() {
     const count = this.getMsgCount()
     if (count > 0) {
       this.getMsg()
+    }
+  },
+  watch: {
+    messages () {
+      if (this.messages.length === 0) {
+        this.visible = false;
+      }
     }
   }
 }
@@ -85,6 +94,8 @@ export default {
 .msgAll {
   float: right;
   margin-right: 20px;
+  color: #409EFF;
+  text-decoration: blink;
 }
 .slide-fade-enter-active {
   transition: all .8s ease;
